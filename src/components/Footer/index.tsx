a89edb0d167841290adb0f3a794018cc7661db8f
Maria Angelova <angelova.m@hotmail.com>
2023-09-09 20:56:32 +0200

    feat: add footer, styles & logosvg

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,26 @@
+import styles from './Footer.module.scss';
+import { LogoLight } from '../Header/LogoLight';
+
 export const Footer = () => {
     return (
-        <footer>
-            
+        <footer className={styles.footer}>
+            <a href="#home">
+                <LogoLight />
+            </a>
+
+            <ul className={styles.menu}>
+                <li className={styles.menuButton}>
+                    <a className={styles.menuLink} href="#home">Home</a>
+                </li>
+                <li className={styles.menuButton}>
+                    <a className={styles.menuLink} href="#about">About</a>
+                </li>
+                <li className={styles.menuButton}>
+                    <a className={styles.menuLink} href="#case">Work</a>
+                </li>
+            </ul>
+
+            <p className={styles.copyRight}>Copyright © 2023 Julia Savchuk.</p>
         </footer>
     )
 }
