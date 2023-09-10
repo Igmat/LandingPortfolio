b1120d52eaf07fc36e4bf2d71ffdb55c94087b32
Maria Angelova <angelova.m@hotmail.com>
2023-09-10 20:03:45 +0200

    feat: add Case component

--- src/components/Main/Case/index.tsx
+++ src/components/Main/Case/index.tsx
@@ -1,7 +1,15 @@
+import { Button } from '../../Button';
+import background from '../../../images/Auction-app.png'
+import styles from './Case.module.scss';
+
 export const Case = () => {
     return (
-        <div>
-            
-        </div>
+        <section className={styles.sectionWrapper}>
+            <div className={styles.sectionTextWrapper}>
+                <h3>Crypto Startup with Streaming</h3>
+                <p>Exploring the fusion of streaming and auctions, this project showcases a unique platform for content creators to connect with their audience. At its core, a custom crypto token empowers users to bid on exclusive content and engage with their favorite creators.</p>
+                <Button>View Case Study</Button>
+            </div>
+        </section>
     )
 }
