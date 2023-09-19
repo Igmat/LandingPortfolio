30ee0115efc923bbb1c35b1a207b4bb7b23b9979
Ihor Chulinda <ichulinda@gmail.com>
2023-09-19 14:24:16 +0200

    fix: proper name for static files

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -406,7 +406,7 @@ module.exports = function (webpackEnv) {
               use: {
                 loader: 'file-loader',
                 options: {
-                  name: '[name].[ext]',
+                  name: '[path][name].[ext]',
                 },
               },
             },
